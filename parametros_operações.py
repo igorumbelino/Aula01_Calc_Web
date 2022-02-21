b16b6558cc9a19c889139f364af6a179f85093b9
igorumbelino <100170047+igorumbelino@users.noreply.github.com>
2022-02-21 20:46:57 -0300

    Add files via upload

---
+++ parametros_operações.py
@@ -0,0 +1,14 @@
+# Parametros_Operação
+
+
+def  adicao ( num1 , num2 ):
+    retornar  num1  +  num2
+
+def  subtração ( num1 , num2 ):
+    retornar  num1  -  num2
+
+def  multiplicacao ( num1 , num2 ):
+    retornar  num1  *  num2
+
+def  divisão ( num1 , num2 ):
+    retornar  num1  /  num2
